216891e3f06a4eefa8c9c9000c926f56258bc2ef
Joar Wathne <wathne@users.noreply.github.com>
2023-09-22 21:27:15 +0200

    feat(lab3): complete errors/errors.go
    modified:   lab3/errors/errors.go

--- lab3/errors/errors.go
+++ lab3/errors/errors.go
@@ -1,5 +1,9 @@
 package errors
 
+import (
+	"fmt"
+)
+
 /*
 Task 5: Errors needed for multiwriter
 
@@ -9,9 +13,10 @@ http://blog.golang.org/error-handling-and-go
 Similar to a the Stringer interface, the error interface also defines a
 method that returns a string.
 
-type error interface {
-    Error() string
-}
+// Note(wathne): Added extra comments. The linter would misinterpret this part.
+// type error interface {
+//     Error() string
+// }
 
 Thus also the error type can describe itself as a string. The fmt package (and
 many others) use this Error() method to print errors.
@@ -37,5 +42,26 @@ The first error + " (and 1 other error)"
 The first error + " (and X other errors)"
 */
 func (m Errors) Error() string {
-	return ""
+	if m == nil {
+		return "(0 errors)"
+	}
+	validErrors := make([]error, 0)
+	for _, v := range m {
+		if v == nil {
+			continue
+		}
+		validErrors = append(validErrors, v)
+	}
+	errorCount := len(validErrors)
+	if errorCount == 1 {
+		return validErrors[0].Error()
+	}
+	if errorCount == 2 {
+		return fmt.Sprint(validErrors[0], " (and 1 other error)")
+	}
+	if errorCount > 2 {
+		return fmt.Sprint(validErrors[0], " (and ", errorCount-1,
+			" other errors)")
+	}
+	return "(0 errors)"
 }
