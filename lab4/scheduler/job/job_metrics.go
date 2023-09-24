96b257b33ba33b7ddb60ed6fc625e509c143daff
Joar Wathne <wathne@users.noreply.github.com>
2023-09-24 22:50:12 +0200

    feat(lab4): implement job metrics
    modified:   scheduler/job/job_metrics.go

--- lab4/scheduler/job/job_metrics.go
+++ lab4/scheduler/job/job_metrics.go
@@ -6,20 +6,31 @@ import (
 )
 
 func (j *Job) Scheduled(s systime.SystemTime) {
+	// How can job.Job call Now()? Rhetorical question.
+	// systime.SystemTime interface is embedded in job.Job struct.
+	// systime.SystemTime interface is implemented by system.System struct.
+	// system.System.Run() calls job.Job.Scheduled() and pass a system.System
+	// (itself) as the argument. See below, as we set the embedded interface.
 	j.SystemTime = s
-	// TODO(student) implement task 2.1
+	// Implemented task 2.1
+	j.arrival = j.Now()
 }
 
 func (j *Job) Started(cpuID int) {
-	// TODO(student) implement task 2.2
+	// Implemented task 2.2
+	if j.start == NotStartedYet {
+		j.start = j.Now()
+	}
 }
 
 func (j Job) TurnaroundTime() time.Duration {
-	// TODO(student) implement task 2.3
-	return 0
+	// Implemented task 2.3
+	tt := j.finished - j.arrival
+	return tt
 }
 
 func (j Job) ResponseTime() time.Duration {
-	// TODO(student) implement task 2.3
-	return 0
+	// Implemented task 2.3
+	rt := j.start - j.arrival
+	return rt
 }
