ce4fa185a38eb4d1cddadaf1e2e079804dbce621
Vahid <73879278+VHPL-UIS@users.noreply.github.com>
2023-09-25 11:19:48 +0200

    Update errors.go - better explanation for more than two errors.

--- lab3/errors/errors.go
+++ lab3/errors/errors.go
@@ -32,7 +32,7 @@ The error string return by the corresponding Error() method.
 
 The first error + " (and 1 other error)"
 
-4. When there are X>1 errors in the slice, it should return:
+4. When there are more than two errors in the slice (first + X; where X>1), it should return:
 
 The first error + " (and X other errors)"
 */
