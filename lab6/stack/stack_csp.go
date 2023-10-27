bb9ddc48a85caa0acaf3dd5fe7f9aaa28b7d7792
Joar Wathne <wathne@users.noreply.github.com>
2023-10-27 14:51:13 +0200

    feat(lab6/stack): implement SafeStack and CspStack
    modified:   lab6/stack/stack_csp.go
    modified:   lab6/stack/stack_sync.go

--- lab6/stack/stack_csp.go
+++ lab6/stack/stack_csp.go
@@ -9,39 +9,100 @@ const (
 )
 
 type stackCommand struct {
+	// Add necessary fields.
 	op     stackOperation
-	// TODO(student) Add necessary fields
+	sender chan<- interface{}
+	value  interface{}
 }
 
 // CspStack is a struct with methods needed to implement the Stack interface.
 type CspStack struct {
-	size int
-	// TODO(student) Add necessary fields
+	// Add necessary fields.
+	receiver <-chan stackCommand
+	sender   chan<- stackCommand
+	size     int
+	top      *Element
 }
 
 // NewCspStack returns an empty CspStack.
 func NewCspStack() *CspStack {
-	// TODO(student) Implement constructor and start handling commands
-	return &CspStack{}
+	// Implement constructor and start handling commands.
+	var channel chan stackCommand = make(chan stackCommand)
+	var receiver <-chan stackCommand = channel
+	var sender chan<- stackCommand = channel
+	var cs *CspStack = &CspStack{
+		receiver: receiver,
+		sender:   sender,
+		size:     0,
+		top:      nil,
+	}
+	go cs.run()
+	return cs
 }
 
 // Size returns the size of the stack.
 func (cs *CspStack) Size() int {
-	// TODO(student) Implement size
-	return -1
+	// Implement size.
+	var channel chan interface{} = make(chan interface{})
+	var receiver <-chan interface{} = channel
+	var sender chan<- interface{} = channel
+	cs.sender <- stackCommand{
+		op:     length,
+		sender: sender,
+		value:  nil,
+	}
+	return (<-receiver).(int)
 }
 
 // Push pushes value onto the stack.
 func (cs *CspStack) Push(value interface{}) {
-	// TODO(student) Implement push
+	// Implement push.
+	var channel chan interface{} = make(chan interface{})
+	var receiver <-chan interface{} = channel
+	var sender chan<- interface{} = channel
+	cs.sender <- stackCommand{
+		op:     push,
+		sender: sender,
+		value:  value,
+	}
+	<-receiver
 }
 
 // Pop pops the value at the top of the stack and returns it.
 func (cs *CspStack) Pop() (value interface{}) {
-	// TODO(student) Implement pop
-	return nil
+	// Implement pop.
+	var channel chan interface{} = make(chan interface{})
+	var receiver <-chan interface{} = channel
+	var sender chan<- interface{} = channel
+	cs.sender <- stackCommand{
+		op:     pop,
+		sender: sender,
+		value:  nil,
+	}
+	return <-receiver
 }
 
 func (cs *CspStack) run() {
-	// TODO(student) Implement handlers for each stack command
+	// Implement handlers for each stack command.
+	var cmd stackCommand
+	for cmd = range cs.receiver {
+		switch cmd.op {
+		case length:
+			cmd.sender <- cs.size
+		case push:
+			cs.top = &Element{cmd.value, cs.top}
+			cs.size++
+			cmd.sender <- nil
+		case pop:
+			var value interface{}
+			if cs.size > 0 {
+				value, cs.top = cs.top.value, cs.top.next
+				cs.size--
+				cmd.sender <- value
+			} else {
+				cmd.sender <- nil
+			}
+		}
+		close(cmd.sender)
+	}
 }
--- lab6/stack/stack_sync.go
+++ lab6/stack/stack_sync.go
@@ -1,26 +1,36 @@
 package stack
 
-// TODO(student) Add necessary fields and synchronization primitives
+// Add necessary fields and synchronization primitives.
+import (
+	"sync"
+)
 
 // SafeStack holds the top element of the stack and its size.
 type SafeStack struct {
-	top  *Element
-	size int
+	mutex sync.Mutex
+	size  int
+	top   *Element
 }
 
 // Size returns the size of the stack.
 func (ss *SafeStack) Size() int {
+	defer ss.mutex.Unlock()
+	ss.mutex.Lock()
 	return ss.size
 }
 
 // Push pushes value onto the stack.
 func (ss *SafeStack) Push(value interface{}) {
+	defer ss.mutex.Unlock()
+	ss.mutex.Lock()
 	ss.top = &Element{value, ss.top}
 	ss.size++
 }
 
 // Pop pops the value at the top of the stack and returns it.
 func (ss *SafeStack) Pop() (value interface{}) {
+	defer ss.mutex.Unlock()
+	ss.mutex.Lock()
 	if ss.size > 0 {
 		value, ss.top = ss.top.value, ss.top.next
 		ss.size--
